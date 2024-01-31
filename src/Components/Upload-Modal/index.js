99a715e80eb88c3de46e4ba4c3d94827d1d89eb1
Okusindja de Almeida <okusindja@gmail.com>
2024-01-31 22:54:28 +0100

    🚧 feat: wip

--- src/Components/Upload-Modal/index.js
+++ src/Components/Upload-Modal/index.js
@@ -8,14 +8,13 @@ import {
   TextInput,
 } from "react-native";
 import OutsidePressHandler from "react-native-outside-press";
-import { PdfSVG, TimesSVG } from "../svg";
+import { PdfSVG, TimesSVG, UploadSVG } from "../svg";
 import { useState } from "react";
 import * as DocumentPicker from "expo-document-picker";
 import { ref, uploadBytes, getDownloadURL, storageRef } from "firebase/storage";
 import { FIREBASE_STORAGE } from "../../../FirebaseConfig";
-import { FIREBASE_DB } from '../../../FirebaseConfig';
-import { collection, addDoc } from 'firebase/firestore';
-
+import { FIREBASE_DB } from "../../../FirebaseConfig";
+import { collection, addDoc } from "firebase/firestore";
 
 const UploadModal = ({ folderId, handleClose, refetch }) => {
   const height = useWindowDimensions().height;
@@ -33,26 +32,29 @@ const UploadModal = ({ folderId, handleClose, refetch }) => {
         console.error("Folder ID is undefined.");
         return;
       }
-  
-      const fileRef = ref(FIREBASE_STORAGE, `folders/${folderId}/files/${document.name}`);
-  
+
+      const fileRef = ref(
+        FIREBASE_STORAGE,
+        `folders/${folderId}/files/${document.name}`
+      );
+
       const response = await fetch(document.uri);
       const blob = await response.blob();
-  
+
       await uploadBytes(fileRef, blob);
-  
+
       const downloadURL = await getDownloadURL(fileRef);
-  
+
       return downloadURL; // Return the URL for both thumbnail and video
     } catch (error) {
       console.error("Error uploading file:", error);
       return null; // Return null if there is an error
     }
   };
-  
+
   const saveVideoData = async (videoData) => {
     try {
-      const docRef = await addDoc(collection(FIREBASE_DB, 'videos'), videoData);
+      const docRef = await addDoc(collection(FIREBASE_DB, "videos"), videoData);
       console.log("Document written with ID: ", docRef.id);
       return docRef.id; // Returning the document ID for any further use
     } catch (e) {
@@ -60,7 +62,6 @@ const UploadModal = ({ folderId, handleClose, refetch }) => {
       throw e; // Re-throw the error for calling function to handle
     }
   };
-
 
   const selectDoc = async () => {
     try {
@@ -99,16 +100,16 @@ const UploadModal = ({ folderId, handleClose, refetch }) => {
 
   const handleUpload = async () => {
     console.log("Selected documents for upload:", selectedDocuments); // Debug log
-  
+
     let videoFileURL = "";
     let thumbnailURL = "";
-  
+
     if (selectedDocuments && selectedDocuments.length > 0) {
       for (const document of selectedDocuments) {
         if (verifyFileSize(document)) {
           const isThumbnail = document === thumbnail; // Check if the current document is the thumbnail
           const uploadedURL = await uploadFile(document, isThumbnail);
-  
+
           if (isThumbnail) {
             thumbnailURL = uploadedURL;
           } else if (isVideo) {
@@ -120,7 +121,7 @@ const UploadModal = ({ folderId, handleClose, refetch }) => {
       Alert.alert("No File Selected", "Please select a file to upload.");
       return;
     }
-  
+
     if (isVideo) {
       // Update video metadata with the video and thumbnail URLs
       const videoData = {
@@ -128,19 +129,15 @@ const UploadModal = ({ folderId, handleClose, refetch }) => {
         genre: videoGenre,
         description: "Your video description here", // Replace with actual description
         url: videoFileURL,
-        thumbnailURL: thumbnailURL
+        thumbnailURL: thumbnailURL,
       };
-  
+
       await saveVideoData(videoData);
     }
-  
+
     refetch();
-    Alert.alert(
-      "Upload Successful",
-      "Files have been uploaded successfully."
-    );
+    Alert.alert("Upload Successful", "Files have been uploaded successfully.");
   };
-  
 
   const selectThumbnail = async () => {
     const result = await DocumentPicker.getDocumentAsync({ type: "image/*" });
@@ -149,60 +146,64 @@ const UploadModal = ({ folderId, handleClose, refetch }) => {
     }
   };
   return (
-    <>
-      <OutsidePressHandler
-        onOutsidePress={handleClose}
-        style={[{ width: width, height: height }, styles.modalContainer]}
+    <View style={[styles.modalContainer, { height: height }]}>
+      <View
+        style={
+          //  width: width, height: height },
+          styles.modal
+        }
       >
-        <View style={styles.modal}>
-          <View style={styles.closeBtn}>
-            <Pressable onPress={handleClose}>
-              <TimesSVG maxWidth={60} maxHeight={60} width="100%" />
+        <View style={styles.closeBtn}>
+          <Pressable onPress={handleClose}>
+            <TimesSVG maxWidth={60} maxHeight={60} width="100%" />
+          </Pressable>
+        </View>
+        <Pressable style={styles.fileContainer} onPress={selectDoc}>
+          <UploadSVG maxWidth={65} maxHeight={65} width="100%" />
+          <Text style={styles.fileText}>
+            Clique para selecionar ficheiro(s)
+          </Text>
+        </Pressable>
+
+        <Pressable
+          onPress={() => {
+            handleUpload(selectedDocuments);
+          }}
+          style={[
+            styles.button,
+            {
+              backgroundColor: selectedDocuments ? "#567DF4" : "#C4C4C4",
+            },
+          ]}
+        >
+          <Text style={styles.textButton}>
+            Carregar ficheiro
+            {selectedDocuments.length > 1 ? "s" : ""}
+          </Text>
+        </Pressable>
+        {isVideo && (
+          <View style={styles.videoInfoForm}>
+            <TextInput
+              style={styles.input}
+              placeholder="Name"
+              value={videoName}
+              onChangeText={setVideoName}
+            />
+            <TextInput
+              style={styles.input}
+              placeholder="Genre"
+              value={videoGenre}
+              onChangeText={setVideoGenre}
+            />
+            <Pressable onPress={selectThumbnail}>
+              <Text style={{ color: "#567DF4", fontWeight: "bold" }}>
+                Escolha uma capa
+              </Text>
             </Pressable>
           </View>
-          <Pressable style={styles.fileContainer} onPress={selectDoc}>
-            <PdfSVG maxWidth={65} maxHeight={65} width="100%" />
-            <Text style={styles.fileText}>Select</Text>
-          </Pressable>
-
-          <Pressable
-            onPress={() => {
-              handleUpload(selectedDocuments);
-            }}
-            style={[
-              styles.button,
-              {
-                backgroundColor: selectedDocuments ? "#567DF4" : "#C4C4C4",
-              },
-            ]}
-          >
-            <Text style={styles.textButton}>
-              Carregar ficheiro
-              {selectedDocuments.length > 1 ? "s" : ""}
-            </Text>
-          </Pressable>
-          {isVideo && (
-            <View style={styles.videoInfoForm}>
-              <TextInput
-                style={styles.input}
-                placeholder="Name"
-                value={videoName}
-                onChangeText={setVideoName}
-              />
-              <TextInput
-                style={styles.input}
-                placeholder="Genre"
-                value={videoGenre}
-                onChangeText={setVideoGenre}
-              />
-              <Pressable onPress={selectThumbnail}>
-                <Text>Select Thumbnail</Text>
-              </Pressable>
-            </View>
-          )}
-        </View>
-      </OutsidePressHandler>
-    </>
+        )}
+      </View>
+    </View>
   );
 };
 
@@ -216,8 +217,8 @@ const styles = StyleSheet.create({
   },
   modalContainer: {
     left: 0,
-    bottom: 0,
-    height: "100%",
+    top: 0,
+    width: "100%",
     position: "absolute",
     alignItems: "center",
     justifyContent: "center",
@@ -230,13 +231,11 @@ const styles = StyleSheet.create({
   },
   modal: {
     gap: 30,
-    flex: 1,
     width: "80%",
-    height: "45%",
+    height: 350,
     paddingTop: 50,
     flexWrap: "wrap",
     borderRadius: 30,
-    flexDirection: "row",
     position: "relative",
     alignItems: "center",
     alignContent: "center",
